6205f1307b7ba1a84f11e1f13c19f77886c4dc38
Paparellav <vitantoniopaparella@gmail.com>
2022-05-09 13:47:59 +0200

    milestone 2 completato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
 const app = new Vue(
     {
@@ -25,7 +26,9 @@ const app = new Vue(
             ],
         },
         methods: {
-
+            removeItem: function(index) {
+                this.todos.splice(index, 1)
+            },
         }
     }
 )
